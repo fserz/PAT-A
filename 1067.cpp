de860d678c5b1983f4f355ce076adafa00d6f883
fserz <81620916+fserz@users.noreply.github.com>
2021-07-21 10:39:05 -0500

    1067待修改

---
+++ 1067.cpp
@@ -0,0 +1,42 @@
+# include<cstdio>
+# include<algorithm>
+using namespace std;
+const int maxn = 10010;
+int pos[maxn];
+int main(){
+    int n, ans = 0;
+    scanf("%d", &n);
+    int left = n - 1, num;
+    for (int  i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        pos[num] = i;
+        if(num == i && num != 0)
+        left--;
+    }
+int k =1;
+while (left > 0)
+{
+    if(pos[0] == 0){
+        while (k < n)
+        {
+            if(pos[k] != k)
+            
+            ans++;
+            break;  
+        }
+        k++;
+        
+    }
+}
+    while(pos[0] != 0){
+        swap(pos[0], pos[pos[0]]);
+        ans++;
+        left--;
+    }
+
+
+printf("%d", ans);
+
+    return 0;
+}
