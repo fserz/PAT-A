481142d78f108f8e3c12526dcad0cb691f349fcc
fserz <873444264@qq.com>
2022-04-13 22:42:22 +0800

    数学问题

---
+++ 1003.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int n, m, c1, c2;
+    cin >> n >> m >> c1 >> c2;
+    for (int i = 0; i < n; i++)`{
+        
+    }
+    
+    return 0;
+}
