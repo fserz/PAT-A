3527b93c6314f491aad7073d0301454751d047a2
fserz <873444264@qq.com>
2022-04-19 22:40:26 +0800

    反转链表

--- 1074.cpp
+++ 1074.cpp
@@ -1,67 +1,29 @@
-# include <cstdio>
 # include <iostream>
 # include <algorithm>
 using namespace std;
-const int maxn = 10010;
-struct node
-{
- int data;
- int address;
- int next;
- int order;   
-}nodeunion[maxn];
-bool cmp(node a, node b){
-    return a.order < b.order;
-}
-
 int main(){
-int firstadd, n , k;
-cin >>firstadd >> n >> k;
-for (int i = 0; i < maxn; i++)
-{
-    nodeunion[i].order = maxn;
-}
-int p = firstadd; 
-int count = 0;
-while(nodeunion[p].address != -1){
-    nodeunion[p].order = count;
-    count++;
-}
-sort(nodeunion, nodeunion + k, cmp);
-
-for (int i = 0; i < count / k; i++)
-{
-    for (int j = (i + 1) * k - 1; j > i * k; j--)
+    int first, n, k, temp;
+    cin >> first >> n >> k;
+    int data[100010], next[100010], list[100010];
+    for (int i = 0; i < n; i++)
     {
-       printf("%05d %d %05d", nodeunion[j].address, nodeunion[j].data, nodeunion[j - 1].address);
+        cin >> temp;
+        cin >> data[temp] >> next[temp];
     }
-    printf("%05d %d", nodeunion[i * k].address, nodeunion[i * k].data); //每分组最后一块
-    if (i < count / k - 1)
+    int sum = 0;
+    while (first != -1)
     {
-        printf("%05d\n", nodeunion[(i + 2) * k - 1].address);
+        list[sum++] = first;
+        first = next[first];
     }
-    else
+    for (int i = 0; i < (sum - sum % k); i += k)
     {
-        if (count / k == 0)
-        {
-            printf("-1\n");
-        }else
-        {
-            printf("%05d %d", nodeunion[(i + 1) * k].address, nodeunion[i].data);
-            for (int i = count / k * k; i < count; i++)
-            {
-                printf("%05d %d", nodeunion[i].address,nodeunion[i].data);
-                if (i < count - 1)
-                {
-                   printf("%05d", nodeunion[i + 1].address);
-                }
-                else
-                {
-                    printf("-1");
-                }   
-            }
-        }   
-    }       
-}
+        reverse(list + i, list + i + k);
+    }
+    for (int i = 0; i < sum - 1; i++)
+    {
+        printf("%05d %d %05d\n", list[i], data[list[i]], list[i + 1]);
+    }
+    printf("%05d %d -1", list[sum - 1], data[list[sum - 1]]);
     return 0;
 }
