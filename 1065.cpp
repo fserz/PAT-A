5529d4eabf6a46aeff1e599c5dba390b8a4c2fbe
fserz <873444264@qq.com>
2022-04-21 00:11:58 +0800

    大数相加（溢出）

--- 1065.cpp
+++ 1065.cpp
@@ -1,23 +1,24 @@
-# include <cstdio>
-
+# include <iostream>
+using namespace std;
+int i;
+void comp(long a, long b, long c){
+    long sum = a + b;  
+    if (a > 0 && b > 0 && sum < 0)//正溢
+        cout << "Case #" << i << ": true" << endl;
+    else if(a < 0 && b < 0 && sum >= 0)//负溢为>=0，注意等号
+        cout << "Case #" << i << ": false" << endl;
+    else if(sum > c)
+        cout << "Case #" << i << ": true" << endl;
+    else
+        cout << "Case #" << i << ": false" << endl;
+}
 int main(){
-    int T, tcase = 1;
-    scanf("%d", &T);
-    for(int i = 0; i < T; i++){
-        long long a, b, c;
-        scanf("%lld%lld%lld", &a, &b, &c);
-        bool flag;
-        long long sum = a + b;
-        if(a > 0 && b > 0 && sum < 0) flag = true;
-        else if(a < 0 && b < 0 && sum > 0) flag = false;
-        else if(sum > c) flag = true;
-        else flag = false;
-        if(flag == true){
-        printf("Case #%d: true\n", tcase);
-        }else{
-           printf("Case #%d: false\n", tcase);
-        }
-        ++tcase;
+    int n;
+    long long a, b, c;
+    cin >> n;
+    for (i = 1; i <= n; i++){
+        scanf("%lld%lld%lld", &a, &b, &c);//此处数据有可能溢出，需用scanf
+        comp(a, b ,c);
     }
-    return 0;
+return 0;
 }
